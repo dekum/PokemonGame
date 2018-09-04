8ed123d67b678015d0732ca941b0fb74cd703539
dekum <42811110+dekum@users.noreply.github.com>
2018-09-04 08:37:57 -0400

    Update P2Turn.java
    Added new pkmn attacks

--- src/P2Turn.java
+++ src/P2Turn.java
@@ -13,7 +13,7 @@ public class P2Turn {
         int randoomInt = random.nextInt(4) +1; //Random code includes 0 so range is 0,1,2,3 so I want to add 1 so range is 1 thru 4
 
         //Should make pokmn ID to make this easy but I know were using squrtle for test
-        randoomInt = 1;
+        
         if (randoomInt == 1)
         {
             //Tackle Attack
@@ -23,8 +23,38 @@ public class P2Turn {
 
 
         }
+        
+        if (randoomInt == 2)
+        {
+            //Water Gun
+            System.out.println(P2.getName() + "is using Bubble!");
+            CalculateDamange calc1 = new CalculateDamange(P2, P1, 20, 1);
+            //So lets see how easy Java switches P1 and P2 stats, could save alot of time
 
-        if(P1.getHP() <= 0)
+
+        }
+        
+        if (randoomInt == 3)
+        {
+            //Hydro Pump
+            System.out.println(P2.getName() + "is using Hydro Pump!");
+            CalculateDamange calc1 = new CalculateDamange(P2, P1, 35, 1);
+            //So lets see how easy Java switches P1 and P2 stats, could save alot of time
+
+
+        }
+        
+        if (randoomInt == 4)
+        {
+            //WithDrawl
+            System.out.println(P2.getName() + "is using Withdrawl!");
+            P2.setDef(P2.getDef()+ 15);
+            //So lets see how easy Java switches P1 and P2 stats, could save alot of time
+
+
+        }
+
+        if(P1.getHP() <= 0) //Check if p1 pkmn fainted
         {
             System.out.println("End of battle you lose! Try again!");
             System.exit(0);
